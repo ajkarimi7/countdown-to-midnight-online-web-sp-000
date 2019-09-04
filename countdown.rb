35475447f5dffbb910b1cc9bb9a4125f9e7a569c
Ali Karimi <ajkarimi7@gmail.com>
2019-09-04 18:38:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,9 @@
 
 def countdown(number)
   number = 10 
-  while number > 0  
+  while number > 0
+    number -= 1
     puts "#{number} SECOND(S)!"
-    number -= 1 
   end 
   
   puts "HAPPY NEW YEAR!"
