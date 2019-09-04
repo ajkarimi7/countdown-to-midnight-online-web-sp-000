01cae703a6a6789452023a622135432714c4a32c
Ali Karimi <ajkarimi7@gmail.com>
2019-09-04 18:43:13 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,7 +6,13 @@ def countdown(seconds_to_midnight)
     puts "#{seconds_to_midnight} SECOND(S)!"
     number -= 1
   end 
-  
    "HAPPY NEW YEAR!"
-  
+end
+
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
